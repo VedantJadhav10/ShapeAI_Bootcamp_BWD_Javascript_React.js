5faf0433ade6bb961f4d4b63e0a0b6dc97b8953b
VedantJadhav10 <85446764+VedantJadhav10@users.noreply.github.com>
2021-06-06 20:21:25 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note()
+{
+  return (
+    <div className="note">
+        <h1>Javascript & React.js</h1>
+        <p>
+            This was an amazing bootcamp 
+            where I really learn from scratch by
+            sir Shaurya Sinha. It was a great experince.
+            Thank you.  
+        </p> 
+    </div>
+  );
+}
+
+export default Note;
